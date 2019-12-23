e8054686722c0b90ab9b46642f1d46b857184c87
Ethan Nguyen <ethanenguyen@hotmail.com>
2019-12-23 10:54:10 -0800

    Added blogging and polling apps

---
+++ mysite/blogging/tests.py
@@ -0,0 +1,14 @@
+from blogging.models import Post  # add this import at the top
+from django.test import TestCase
+
+
+# Create your tests here.
+class PostTestCase(TestCase):
+    # ...
+
+    # add this test method to the PostTestCase
+    def test_string_representation(self):
+        expected = "This is a title"
+        p1 = Post(title=expected)
+        actual = str(p1)
+        self.assertEqual(expected, actual)
